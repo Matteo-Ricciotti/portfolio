663032c56702e9a1cb8cd9796d982acae2c69566
Matteo <ricciottimatteo2003@gmail.com>
2023-12-02 08:33:16 +0100

    fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,10 +6,10 @@ import {
 } from "./canvas";
 import Hero from "./main/Hero";
 import About from "./main/About";
-import Tech from "./Tech";
-import Experience from "./Experience";
+import Tech from "./work/Tech";
+import Experience from "./work/Experience";
 import Works from "./main/Works";
-import Feedbacks from "./Feedbacks";
+import Feedbacks from "./work/Feedbacks";
 import Contact from "./main/Contact";
 import Navbar from "./navbar";
 
