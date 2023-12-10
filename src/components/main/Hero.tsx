15e9a4b002d337f8d479d50fcce18c30b42d6317
Matteo <ricciottimatteo2003@gmail.com>
2023-12-10 14:54:56 +0100

    optimization

--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
       <div className="absolute bottom-32 flex w-full items-center justify-center xs:bottom-10">
-        <a href="#about">
+        <a href="#about" title="About">
           <div className="flex h-16 w-[35px] items-start justify-center rounded-3xl border-4 border-secondary p-2">
             <motion.div
               animate={{
