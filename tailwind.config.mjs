d2560279e91809b3f903b3dcf7b1cefb96fd4646
Matteo Ricciotti <ricciottimatteo2003@gmail.com>
2024-08-18 17:34:24 +0200

    main layout

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.astro'],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
