1194f8afea99d51fa68f30471b722d3f737713b5
Matteo <ricciottimatteo2003@gmail.com>
2024-08-20 18:04:02 +0200

    minor adjustemts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.astro'],
-  theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-      },
+    content: ["./src/**/*.astro"],
+    theme: {
+        container: {
+            center: true,
+            padding: "1rem",
+            screens: {
+                sm: "640px",
+                md: "768px",
+                lg: "960px",
+                xl: "1200px",
+            },
+        },
+        extend: {
+            colors: {
+                primary: "#1c1c22",
+                accent: {
+                    DEFAULT: "#00ff99",
+                    hover: "#00ff9990",
+                },
+            },
+        },
     },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 };
