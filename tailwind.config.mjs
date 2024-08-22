e03eba1c8913a463c48e0de4ddf3d78492963347
Matteo <ricciottimatteo2003@gmail.com>
2024-08-22 17:29:00 +0200

    home responsiveness fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,12 @@ export default {
                 xl: "1200px",
             },
         },
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "960px",
+            xl: "1200px",
+        },
         extend: {
             colors: {
                 primary: "#1c1c22",
