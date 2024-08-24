5283db7562e2abf6c8f74664b4f30fecd637fb7e
Matteo <ricciottimatteo2003@gmail.com>
2024-08-24 13:33:13 +0200

    basic work page

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -4,34 +4,55 @@ export const WORK = [
         name: "Project 1",
         description: "Description 1",
         image: "sample.jpg",
+        stack: ["react-js", "node-js", "typescript"],
     },
     {
         code: "project-2",
         name: "Project 2",
         description: "Description 2",
         image: "sample.jpg",
+        stack: ["react-js", "node-js", "typescript"],
     },
     {
         code: "project-3",
         name: "Project 3",
         description: "Description 3",
         image: "sample.jpg",
+        stack: ["react-js", "node-js", "typescript"],
     },
     {
         code: "project-4",
         name: "Project 4",
         description: "Description 4",
         image: "sample.jpg",
+        stack: ["react-js", "node-js", "typescript"],
     },
     {
         code: "project-5",
         name: "Project 5",
         description: "Description 5",
         image: "sample.jpg",
+        stack: ["react-js", "node-js", "typescript"],
     },
 ];
 
-export const SKILLS = [];
+export const STACK = [
+    {
+        id: "react-js",
+        name: "ReactJS",
+        url: "https://react.dev/",
+    },
+    {
+        id: "node-js",
+        name: "NodeJS",
+        url: "https://nodejs.org",
+    },
+    {
+        id: "typescript",
+        name: "Typescript",
+        url: "https://www.typescriptlang.org/",
+    },
+];
 
 export const CONTACTS = [];
 
