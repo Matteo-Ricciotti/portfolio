373f2ef664e631012db857e55f0da549dbb66d68
Matteo <ricciottimatteo2003@gmail.com>
2024-08-24 12:25:23 +0200

    image optimization

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -3,31 +3,31 @@ export const PROJECTS = [
         code: "project-1",
         name: "Project 1",
         description: "Description 1",
-        image: "/images/sample.jpg",
+        image: "sample.jpg",
     },
     {
         code: "project-2",
         name: "Project 2",
         description: "Description 2",
-        image: "/images/sample.jpg",
+        image: "sample.jpg",
     },
     {
         code: "project-3",
         name: "Project 3",
         description: "Description 3",
-        image: "/images/sample.jpg",
+        image: "sample.jpg",
     },
     {
         code: "project-4",
         name: "Project 4",
         description: "Description 4",
-        image: "/images/sample.jpg",
+        image: "sample.jpg",
     },
     {
         code: "project-5",
         name: "Project 5",
         description: "Description 5",
-        image: "/images/sample.jpg",
+        image: "sample.jpg",
     },
 ];
 
