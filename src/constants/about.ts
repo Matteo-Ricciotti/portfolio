ab421a68db136c8f8e7863ef915512f900cf6cc1
Matteo <ricciottimatteo2003@gmail.com>
2024-09-04 15:14:52 +0200

    about page

---
+++ src/constants/about.ts
@@ -0,0 +1,10 @@
+export const ABOUT = {
+    NAME: "Matteo",
+    SURNAME: "Ricciotti",
+    EMAIL: "ricciottimatteo2003@gmail.com",
+    NATIONALITY: "Italian",
+    CURRENT_LOCATION: "Turin (Italy)",
+    LANGUAGES: ["Italian", "English"],
+    FREELANCE: true,
+    START_CAREER_TIMESTAMP: 1664787600000, // Monday October 3rd 2022 09:00:00
+};
--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,34 @@ import clsx from "clsx";
 import { twMerge, type ClassNameValue } from "tailwind-merge";
 
 export const cn = (...inputs: ClassNameValue[]) => twMerge(clsx(...inputs));
+
+export const getDuration = (timestamp: number) => {
+    const now = Date.now();
+    const diff = now - timestamp;
+
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const years = Math.floor(days / 365);
+
+    const remainingDaysAfterYear = days % 365;
+    const remainingMonths = Math.floor(remainingDaysAfterYear / 30);
+    const remainingDaysAfterMonth = remainingDaysAfterYear % 30;
+
+    const remainingHours = hours % 24;
+    const remainingMinutes = minutes % 60;
+    const remainingSeconds = seconds % 60;
+
+    const yearsStr = years > 0 ? `${years} Year ` : "";
+    const monthsStr = remainingMonths > 0 ? `${remainingMonths} Months ` : "";
+    const daysStr = remainingDaysAfterMonth > 0 ? `${remainingDaysAfterMonth} Days ` : "";
+    const hoursStr = remainingHours > 0 ? `${remainingHours} Hours ` : "";
+    const minutesStr = remainingMinutes > 0 ? `${remainingMinutes} Minutes ` : "";
+    const secondsStr = remainingSeconds > 0 ? `${remainingSeconds} Seconds` : "";
+
+    return {
+        partial: `${yearsStr}${monthsStr}`.trim() + "...",
+        full: `${yearsStr}${monthsStr}${daysStr}${hoursStr}${minutesStr}${secondsStr}`.trim(),
+    };
+};
