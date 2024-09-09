554c5166d594beee39e029078ce7f57d93f82afd
Matteo <ricciottimatteo2003@gmail.com>
2024-09-09 20:06:58 +0200

    link to company website

--- src/components/react/job-card.tsx
+++ src/components/react/job-card.tsx
@@ -2,9 +2,12 @@ import type { Job } from "@/constants/jobs";
 
 type JobCardProps = Job;
 
-export const JobCard = ({ role, company, startDate, endDate }: JobCardProps) => {
+export const JobCard = ({ role, company, website, startDate, endDate }: JobCardProps) => {
     return (
-        <div className="group rounded-md bg-black/20 p-4 text-center transition-all hover:bg-white/5 sm:p-6 md:text-start md:hover:scale-[1.05]">
+        <button
+            className="group rounded-md bg-black/20 p-4 text-center transition-all hover:cursor-pointer hover:bg-white/5 sm:p-6 md:text-start md:hover:scale-[1.05]"
+            onClick={() => window && window.open(website)}
+        >
             <p className="text-sm tracking-wide text-accent">
                 {startDate} - {endDate || "Now"}
             </p>
@@ -13,6 +16,6 @@ export const JobCard = ({ role, company, startDate, endDate }: JobCardProps) => 
                 <div className="size-1.5 rounded-full bg-accent"></div>
                 <p className="tracking-widest text-white/50">{company}</p>
             </div>
-        </div>
+        </button>
     );
 };
--- src/constants/jobs.ts
+++ src/constants/jobs.ts
@@ -1,6 +1,7 @@
 export type Job = {
     role: string;
     company: string;
+    website: string;
     startDate: string;
     endDate: string | undefined;
 };
@@ -9,12 +10,14 @@ export const JOBS: Job[] = [
     {
         role: "Front End Developer",
         company: "Enhancers",
+        website: "https://enhancers.it/en",
         startDate: "May 2023",
         endDate: "July 2023",
     },
     {
         role: "Full Stack Developer",
         company: "Coolshop",
+        website: "https://www.coolshop.it/en",
         startDate: "November 2023",
         endDate: undefined,
     },
